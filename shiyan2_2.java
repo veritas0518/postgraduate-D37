8c37374faf70781097a360a1c16f864c53634439
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-13 12:01:13 +0800

    A 3 × 4 matrix,Matrix transpose, product ,output
    【问题描述】编写程序，输入一个3×4的矩阵A，输出矩阵的转置1.png以及二者的乘积2.png。输出矩阵的各列之间用Tab键对齐，输入输出格式如样例所示。
    提示1：一个m行k列的矩阵A和k行n列的矩阵B的乘积C=AB是一个m行n列的矩阵，其第i行第j列的元素是由A的第i行的全部元素和B的第j列的全部元素对应相乘的和，即：3.png。
    提示2：用三重循环解决，第一层用i遍历C的每一行，第二层用j遍历C的每一列，第三层用k遍历A的第i行每一列和B的第j列每一行求对应元素乘积并累加和作为C[i][j]。
    2.jpg
    【样例输入】
    1 2 3 4
    5 6 7 8
    9 10 11 12
    【样例输出】
    1.png

---
+++ shiyan2_2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class shiyan2_2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int[][]a=new int[3][4];
+        int[][]b=new int[4][3];
+        int[][]c=new int[3][3];
+        for(int i=0;i<3;i++) {
+            for (int j = 0; j < 4; j++){
+                a[i][j]=s.nextInt();
+                b[j][i]=a[i][j];
+
+            }
+        }
+        System.out.printf("Original matrix:\n");
+        for(int i=0;i<3;i++) {
+            for (int j = 0; j < 4; j++){
+              // System.out.printf("Original matrix:\n");
+                System.out.printf("%d\t",a[i][j]);
+            }
+            System.out.println();
+        }
+        //b[4][3]=a[3][4];
+        System.out.printf("Transpose:\n");
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+               //System.out.printf("Transpose:\n");
+                System.out.printf("%d\t",b[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.printf("Matrix multiplication:\n");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                for(int m=0;m<4;m++){
+                    c[i][j]+=a[i][m]*b[m][j];
+
+                }
+                System.out.printf("%d\t",c[i][j]);
+                //System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
